0fa195da4ce19e2acba36fdda3a2015050ce8c22
lilcharles33 <lilcharles33@csm-wl-dhcp-236-162.mines.edu>
2019-03-05 14:47:02 -0700

    make sure this works

---
+++ Documents/eclipse-workspace/ClueGame/src/experiment/BoardCell.java
@@ -0,0 +1,13 @@
+// authors: Cameron Kuan and Nathan Beveridge
+
+package experiment;
+
+public class BoardCell {
+	
+	private int row;
+	private int col;
+	
+	public BoardCell(int row, int col) {
+
+	}
+}
